30f36683d0e59172f39903b28467c398b28d580c
poudelalice <poudelalicex@gmail.com>
2025-10-19 12:24:46 -0400

    added input  event listeners for rgb sliders

--- lab-2/lab-2/js/page-color-changer.js
+++ lab-2/lab-2/js/page-color-changer.js
@@ -3,3 +3,8 @@ const redSlider = document.getElementById('red');
 const greenSlider = document.getElementById('green');
 const blueSlider = document.getElementById('blue');
 const body = document.querySelector('html');
+
+//adding event listeners for running a function whenever a slider value changes
+redSlider.addEventListener('input', changeBackgroundColor);
+greenSlider.addEventListener('input', changeBackgroundColor);
+blueSlider.addEventListener('input', changeBackgroundColor);
