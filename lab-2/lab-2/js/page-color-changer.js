35c9456c4592db27a9d5b0a9e01b6ac6de0a8555
poudelalice <poudelalicex@gmail.com>
2025-10-19 12:22:07 -0400

    declared constants for red, green, blue sliders and html body

---
+++ lab-2/lab-2/js/page-color-changer.js
@@ -0,0 +1,5 @@
+//getting references to all the important elements on the page
+const redSlider = document.getElementById('red');
+const greenSlider = document.getElementById('green');
+const blueSlider = document.getElementById('blue');
+const body = document.querySelector('html');
